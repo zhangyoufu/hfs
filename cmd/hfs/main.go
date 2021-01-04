1b2d91ec580771002f104ca24e5bdfb22d85aa65
张酉夫 <zhangyoufu@gmail.com>
2021-01-04 12:10:35 +0800

    add unix socket and proxy protocol support

--- cmd/hfs/main.go
+++ cmd/hfs/main.go
@@ -3,20 +3,32 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
+	"github.com/c0va23/go-proxyprotocol"
 	"github.com/zhangyoufu/hfs"
 )
 
 func main() {
 	var (
-		addr, root                                                   string
-		accessLog, dirList, dirFirst, ignoreCase, dotFile, indexPage bool
+		network     string
+		address     string
+		root        string
+		enableProxy bool
+		accessLog   bool
+		dirList     bool
+		dirFirst    bool
+		ignoreCase  bool
+		dotFile     bool
+		indexPage   bool
 	)
 
-	flag.StringVar(&addr, "addr", "localhost:8000", "listen `address:port`")
+	flag.StringVar(&network, "network", "tcp", "tcp or unix")
+	flag.StringVar(&address, "addr", "localhost:8000", "address:port for tcp, or filesystem path for unix")
 	flag.StringVar(&root, "root", ".", "`path` of document root")
+	flag.BoolVar(&enableProxy, "proxy", false, "enable PROXY protocol support")
 	flag.BoolVar(&accessLog, "log", true, "enable access log")
 	flag.BoolVar(&dirList, "list", true, "enable directory listing")
 	flag.BoolVar(&dirFirst, "dirfirst", true, "list directories before files")
@@ -38,8 +50,29 @@ func main() {
 		logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds)
 	}
 
-	log.Printf("Serving %s on %s", root, addr)
-	log.Fatal(http.ListenAndServe(addr, &hfs.FileServer{
+	switch network {
+	case "tcp":
+	case "unix":
+		_ = os.Remove(address)
+	default:
+		log.Fatal("unsupported network")
+	}
+
+	rawListener, err := net.Listen(network, address)
+	if err != nil {
+		log.Fatal("unable to listen: ", err)
+	}
+	ln := rawListener.(net.Listener)
+
+	if enableProxy {
+		// DefaultFallbackHeaderParserBuilder contains StubHeaderParserBuilder,
+		// which accepts non-PROXY protocol traffic
+		proxyListener := proxyprotocol.NewDefaultListener(ln)
+		ln = proxyListener
+	}
+
+	log.Printf("Serving %s on [%s] %s", root, network, address)
+	log.Fatal(http.Serve(ln, &hfs.FileServer{
 		FileSystem:       http.Dir(root),
 		Sorter:           sorter,
 		AccessLog:        logger,
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zhangyoufu/hfs
 
 go 1.15
+
+require github.com/c0va23/go-proxyprotocol v0.9.1
